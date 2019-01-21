b8d980bbe0b95c53e8f103d14cd924fb42ca6a76
Johnson Joseph <jhonsonj88@gmail.com>
2019-01-21 06:46:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end 
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_end_of_array.unshift("Los Angeles")
+  array.unshift(element)
   ["New Jersey","New York"]
 end
 
